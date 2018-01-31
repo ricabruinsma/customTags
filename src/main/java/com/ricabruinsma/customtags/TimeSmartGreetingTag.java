cfe219db8a1c48b82e682a485927ff46494a88c0
Rica Bruinsma <rbruinsma@madisoncollege.edu>
2018-01-31 00:29:23 -0600

    day of week added

--- src/main/java/com/ricabruinsma/customtags/TimeSmartGreetingTag.java
+++ src/main/java/com/ricabruinsma/customtags/TimeSmartGreetingTag.java
@@ -4,12 +4,22 @@ import javax.servlet.jsp.JspException;
 import javax.servlet.jsp.JspWriter;
 import javax.servlet.jsp.tagext.SimpleTagSupport;
 import java.io.IOException;
+import java.time.DayOfWeek;
+import java.time.LocalDate;
 
 public class TimeSmartGreetingTag extends SimpleTagSupport {
+
+
+
     @Override
     public void doTag() throws JspException, IOException {
         super.doTag();
         JspWriter out = getJspContext().getOut();
-        out.println("Hello Ent Java Class!  Do you know what time it is???");
+
+        LocalDate date = LocalDate.now();
+        DayOfWeek dayOfWeek = date.getDayOfWeek();
+        out.println("Happy " + dayOfWeek + "!");
+        out.println("Do you know what time it is???");
     }
+
 }
